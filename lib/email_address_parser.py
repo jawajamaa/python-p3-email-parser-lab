2958ff899733c62db13addbe87feaf9b19846f14
Timothy Ryon <tim@timryon.com>
2024-07-11 14:01:26 -0400

    finish and pass all tests

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -7,10 +7,11 @@ class EmailAddressParser:
     def __init__(self, email_addresses):
         self.email_addresses = email_addresses
     
-    # return email list in alphabetical order to pass more tests!!
     def parse(self):
         print("Line 8", type(self.email_addresses))
-        email_list = re.split(r"\s+|,\s*", self.email_addresses)
+        email_list = sorted(re.findall(r"[a-zA-Z0-9]+@+[a-zA-Z0-9]+[.][a-zA-Z0-9]+|[a-zA-Z0-9]+[.-][a-zA-Z0-9]+@+[a-zA-Z0-9]+[.][a-zA-Z0-9]+", self.email_addresses))
+    # line 14 passes all but last test, that includes entries that are not email addresses.  Above solution just finds only email addresses in string and pulls them out instead of using split()
+        # email_list = sorted(re.split(r"\s+|,\s*", self.email_addresses))
         print("Line 13, Email List" , email_list)
         return email_list
 
